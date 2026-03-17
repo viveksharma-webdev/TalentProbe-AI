a5f2787417cc9ade2c1a8c9809ba9726b18082e9
Vivek Sharma <vivek45559s@gmail.com>
2026-03-17 19:46:18 +0530

    Configured cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true,
+}))
 
 /* requiring all the routes*/
 const authRouter = require("./route/authRoutes.js");
