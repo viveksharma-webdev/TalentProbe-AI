075c2aff708f5fb5160615d1d198990e302ce0d0
vivek <vivek45559s@gmail.com>
2026-03-25 19:55:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://talent-probe-ai.vercel.app/",
     credentials: true,
 }))
 
